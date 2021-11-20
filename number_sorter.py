afbcc057f9200c06b19936f86e2a53d1db8c10cb
jendlrs <jensennics.dlrs@gmail.com>
2021-11-20 21:24:49 +0800

    I added the condition for first possible outcome.

--- number_sorter.py
+++ number_sorter.py
@@ -10,6 +10,13 @@ def getNumbers ():
     fourthN= float(input("Enter fourth number: "))
     return firstN, secondN, thirdN, fourthN
 
-first, second, third, forth = getNumbers()
+first, second, third, fourth = getNumbers()
 
 #Step 2: If-else statement to arange the numbers from highest to lowest.
+def displayHTL(first_, second_, third_, fourth_):
+    if (first_ > second_ and first_ > third_ and first_ > fourth_) and (second_ > third_ and second_ > fourth_) and (third_> fourth_):
+        print(f"{first}, {second_}, {third_}, {fourth_}") 
+    else:
+        print("others")
+
+displayHTL(first, second, third, fourth)
