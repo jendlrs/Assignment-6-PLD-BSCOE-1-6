b08535e3de5df63ba658f2e5d9d4e993b944f9ae
jendlrs <jensennics.dlrs@gmail.com>
2021-11-20 22:08:42 +0800

    I inserted different condition when the first number is the highest.

--- number_sorter.py
+++ number_sorter.py
@@ -14,16 +14,32 @@ first, second, third, fourth = getNumbers()
 
 #Step 2: If-else statement to arange the numbers from highest to lowest.
 def displayHTL(first_, second_, third_, fourth_):
-    #first-second-third-fourth
+    #1- first as the highest number
+        #1-2-3-4
     if first_ > second_ > third_ > fourth_:
         print(f"{first}, {second_}, {third_}, {fourth_}") 
-    #second-third-fourth-first
+        #1-2-4-3
+    elif first_ >second_ > fourth_ > third_:
+        print(f"{first_}, {second_}, {fourth_}, {third_}")
+        #1-3-2-4
+    elif first_ > third_ > second_ > fourth_ :
+        print(f"{first_}, {third_}, {second_}, {fourth_}  ")
+        #1-3-4-2
+    elif first_ > third_ > fourth_ > second_: 
+        print(f"{first_}, {third_}, {fourth}, {second_}")
+        #1-4-2-3
+    elif first_ > fourth_ > second_ > third_ :
+        print(f"{first_}, {fourth_}, {second_}, {third_}")
+        #1-4-3-2
+    elif first_ > fourth_ > third_ > fourth_ :
+        print(f"{first_}, {fourth_}, {third_}, {fourth_}")
+    #2- second as the highest number
     elif second_> third_ > fourth_ > first_:
         print(f"{second_}, {third_}, {fourth_}, {first_}")
-    #third-fourth-first-second
+    #3- third as the highest number
     elif third_ > fourth > first_ > second_:
         print(f"{third_}, {fourth_}, {first_}, {second_}") 
-    #fourth-first-second-third
+    #4- fourth  as the highest number
     elif fourth_ > first_ > second_ > third_:
         print(f"{fourth_}, {first_}, {second_}, {third_}")
     else:
