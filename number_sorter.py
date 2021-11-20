3161d205baf5bef9e4e0a7131706fff06b944854
jendlrs <jensennics.dlrs@gmail.com>
2021-11-20 21:00:16 +0800

    I listed the initial steps and other information needed for the program.

---
+++ number_sorter.py
@@ -0,0 +1,6 @@
+#Program 1: Number Sorter
+#Create a program that ask 4 numbers. 
+#Print the 4 numbers from highest to lowest using only if-else statement.
+
+#Step 1: Ask 4 numbers, convert, and store 
+#Step 2: If-else statement to arange the numbers from highest to lowest.
