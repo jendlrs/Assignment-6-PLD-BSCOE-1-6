d9870b204ffb24f08fb089ae5648c6291bbe9043
jendlrs <jensennics.dlrs@gmail.com>
2021-11-20 21:05:57 +0800

    Step 1 is done: Ask 4 numbers, convert and store.

--- number_sorter.py
+++ number_sorter.py
@@ -3,4 +3,13 @@
 #Print the 4 numbers from highest to lowest using only if-else statement.
 
 #Step 1: Ask 4 numbers, convert, and store 
+def getNumbers ():
+    firstN= float(input("Enter first number: "))
+    secondN= float(input("Enter second number: "))
+    thirdN= float(input("Enter third number: "))
+    fourthN= float(input("Enter fourth number: "))
+    return firstN, secondN, thirdN, fourthN
+
+first, second, third, forth = getNumbers()
+
 #Step 2: If-else statement to arange the numbers from highest to lowest.
