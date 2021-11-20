b0300721863756f846ed4285e1b74e670d139266
jendlrs <jensennics.dlrs@gmail.com>
2021-11-20 22:19:33 +0800

    I listed all the possible combination of 4 numbers. The total combination is 24.

--- number_sorter.py
+++ number_sorter.py
@@ -14,6 +14,7 @@ first, second, third, fourth = getNumbers()
 
 #Step 2: If-else statement to arange the numbers from highest to lowest.
 def displayHTL(first_, second_, third_, fourth_):
+
     #1- first as the highest number
         #1-2-3-4
     if first_ > second_ > third_ > fourth_:
@@ -33,15 +34,37 @@ def displayHTL(first_, second_, third_, fourth_):
         #1-4-3-2
     elif first_ > fourth_ > third_ > fourth_ :
         print(f"{first_}, {fourth_}, {third_}, {fourth_}")
+
     #2- second as the highest number
+        #2-1-4-3
     elif second_> third_ > fourth_ > first_:
         print(f"{second_}, {third_}, {fourth_}, {first_}")
+        #2-1-3-4
+        #2-3-4-1
+        #2-3-1-4
+        #2-4-3-1
+        #2-4-1-3
+
     #3- third as the highest number
+        #3-1-2-4
     elif third_ > fourth > first_ > second_:
         print(f"{third_}, {fourth_}, {first_}, {second_}") 
+        #3-1-4-2
+        #3-2-1-4
+        #3-2-4-1
+        #3-4-1-2
+        #3-4-2-1
+
     #4- fourth  as the highest number
+        #4-1-2-3
     elif fourth_ > first_ > second_ > third_:
         print(f"{fourth_}, {first_}, {second_}, {third_}")
+        #4-1-3-2
+        #4-2-3-1
+        #4-2-1-3
+        #4-3-1-2
+        #4-3-2-1
+
     else:
         print("others")
 
