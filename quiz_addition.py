650fb230d8cae9d2a31b93b909eb837fe3ae6ce3
jendlrs <jensennics.dlrs@gmail.com>
2021-11-24 16:54:11 +0800

    I listed the initial steps needed in the program

---
+++ quiz_addition.py
@@ -0,0 +1,12 @@
+#Program 2: Addition Quiz
+#Create a program that automatically generate two random numbers to add (0 to 99)
+#Let the user answer and evaluate if the user has the correct answer
+#The program will ask the user 10 addition operation
+#Display the result summary of the 10 operations (ex 9/10)
+
+#Step 1: Import random
+#Step 2: Ask the user's name and if they want to take the quiz
+#Step #3: If they are ready, proceed in giving two random numbers
+#Step 4: Display total score
+        
+    
