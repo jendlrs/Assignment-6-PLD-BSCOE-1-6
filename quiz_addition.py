456b5630d8f7eb3fba4bfe683edbd3e584a7232d
jendlrs <jensennics.dlrs@gmail.com>
2021-11-24 17:36:01 +0800

    Print the total score.

--- quiz_addition.py
+++ quiz_addition.py
@@ -15,10 +15,10 @@ def getName():
 def given_and_score (nameA):
     ready_or_not = input(f"\nHi, {nameA}! Are you ready to take the quiz? Yes or No: ")
     if ready_or_not == "No" or ready_or_not == "no":
-        print("\nIt's okay, come back when you are ready to take the quiz.")
+        print("\nIt's okay, come back when you are ready to take the quiz.\n")
     else: #Step #3: If they are ready, proceed in giving 2 random numbers
         print("\nIf you are ready, let's proceed.\nThis quiz is composed of 10 questions.\nGood luck!")
-        print("\nInstruction: Find the sum of the following.")
+        print("\nInstruction: Find the sum of the following.\n")
 
         question_number =1 
         user_score =0
@@ -28,7 +28,7 @@ def given_and_score (nameA):
             second_N= random.randrange(0,99)
             answer = first_N + second_N
             
-            print(f"\n{question_number}.) {first_N} + {second_N}")
+            print(f"{question_number}.) {first_N} + {second_N}")
             user_answer = int(input("\nThe sum is: "))
 
         #check if the user's answer is correct
@@ -42,8 +42,8 @@ def given_and_score (nameA):
                 user_score +=0
                 question_number +=1
 
-
 #Step 4: Display total score
+        print(f"Your total score is {user_score}/10")
 
 name= getName()
 given_and_score (name)
