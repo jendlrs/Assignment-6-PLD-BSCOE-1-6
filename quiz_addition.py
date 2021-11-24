1fab2adfc5512d20983a65b69f210719cabe32e4
jendlrs <jensennics.dlrs@gmail.com>
2021-11-24 16:58:59 +0800

    Import random that will be used in step 3. Also added greetings for users.

--- quiz_addition.py
+++ quiz_addition.py
@@ -5,8 +5,10 @@
 #Display the result summary of the 10 operations (ex 9/10)
 
 #Step 1: Import random
+import random #it will be used in step 3
+print("Welcome to Math Quiz! Today's examination is about addition")
 #Step 2: Ask the user's name and if they want to take the quiz
 #Step #3: If they are ready, proceed in giving two random numbers
 #Step 4: Display total score
-        
+
     
